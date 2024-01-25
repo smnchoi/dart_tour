fbe452a1ddc379d83412f06374b87e44552953ae
Soomin Choi <soomin.choi@fphcare.co.nz>
2024-01-25 15:08:14 +1300

    basic data types

--- main.dart
+++ main.dart
@@ -1,4 +1,10 @@
 // Dart goes for main automatically. Always have main.
 void main() {
-  //
+  // Basic Data Types:
+  // All Data Types are Class in Dart.
+  String name = "soomin";
+  bool isMale = true;
+  int age = 26;
+  double pi = 3.14;
+  num x = 12; // num is a parent class of int and double.
 }
