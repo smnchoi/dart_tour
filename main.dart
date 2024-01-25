b571c83eab0e2d0e3fe1bff2769fcc7d2f5e83c5
Soomin Choi <soomin.choi@fphcare.co.nz>
2024-01-25 15:16:05 +1300

    list and collection if

--- main.dart
+++ main.dart
@@ -7,4 +7,23 @@ void main() {
   int age = 26;
   double pi = 3.14;
   num x = 12; // num is a parent class of int and double.
+
+  bool giveMeFive = true;
+
+  // List
+  // var numbers = [1, 2, 3, 4];
+  List<int> numbers = [
+    // same as above.
+    1,
+    2,
+    3,
+    4,
+    if (giveMeFive) 5, // ⬅️ This is "collection if"
+    // Remember to finish the list with `,` this makes list be auto-formatted.
+  ];
+
+  // "collection if" is just same as:
+  // if (giveMeFive) {
+  // numbers.add(5);
+  // }
 }
