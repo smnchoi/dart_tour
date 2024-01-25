58c304e1db5096f8bc20f620fdcaf11b245043fc
Soomin Choi <soomin.choi@fphcare.co.nz>
2024-01-25 14:14:11 +1300

    dynamic type

--- main.dart
+++ main.dart
@@ -1,8 +1,13 @@
 void main() { // Dart goes for main automatically. Always have main.
   print("hello dart"); // semicolon won't be added automatically. there is a specific feature without using semicolons./
 
-
   var name = "name with var";
   // name = false; // NO
   String name2 = "name with explicit type, String";
+
+  var d_name; // "dynamic"
+  dynamic d_name2; // this is also "dynamic"
+  if (d_name2 is String) {
+    // d_name2.codeUnitAt(index)
+  }
 }
