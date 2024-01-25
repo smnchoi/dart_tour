ca2ec02364c7ee4bf3158d7ae26aff7b240ff7a6
Soomin Choi <soomin.choi@fphcare.co.nz>
2024-01-25 14:08:30 +1300

    var v. explicit type keywords

--- main.dart
+++ main.dart
@@ -1,3 +1,8 @@
 void main() { // Dart goes for main automatically. Always have main.
-  print("hello dart");
+  print("hello dart"); // semicolon won't be added automatically. there is a specific feature without using semicolons./
+
+
+  var name = "name with var";
+  // name = false; // NO
+  String name2 = "name with explicit type, String";
 }
